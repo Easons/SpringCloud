7cd82f37f1a4364212ef95ead372b2fb3c0cf464
pangpan <pangpan@asiainfo.com>
2021-11-15 13:31:15 +0800

    提交feignConsumer

--- feignconsumer/src/main/java/com/pangpan/feignconsumer/controller/MainController.java
+++ feignconsumer/src/main/java/com/pangpan/feignconsumer/controller/MainController.java
@@ -1,8 +1,27 @@
 package com.pangpan.feignconsumer.controller;
 
+import com.pangpan.feignconsumer.api.ConsumerApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author pangpan
  * @date 2021-11-14
  */
+@RestController
 public class MainController {
+
+    @Autowired
+    ConsumerApi consumerApi;
+
+
+    @GetMapping("/alive")
+    public String alive(){
+
+        return consumerApi.alive();
+    }
+
+
+
 }
