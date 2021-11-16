7c7545304989643cf0a42f1d533cdd6eed79c8cb
pangpan <pangpan@asiainfo.com>
2021-11-16 10:06:13 +0800

    添加接收多个参数 getMap3

--- feignconsumer/src/main/java/com/pangpan/feignconsumer/controller/MainController.java
+++ feignconsumer/src/main/java/com/pangpan/feignconsumer/controller/MainController.java
@@ -3,7 +3,11 @@ package com.pangpan.feignconsumer.controller;
 import com.pangpan.feignconsumer.api.ConsumerApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author pangpan
@@ -34,5 +38,15 @@ public class MainController {
     }
 
 
-
+    /**
+     * 接收多个参数
+     * @param map
+     * @return
+     */
+    @GetMapping("/getMap3")
+    public Map<Integer, String> getMap3(@RequestParam Map<String, Object> map) {
+        // TODO Auto-generated method stub
+        System.out.println(map);
+        return consumerApi.getMap3(map);
+    }
 }
